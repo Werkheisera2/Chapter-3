f829c0aeee839a39e19acee960765e46ccbfcb77
Werkheisera2 <werkheisera2@gmail.com>
2014-09-15 16:33:25 -0400

    Finished CheckLowRates.cs

---
+++ CheckLowRates/CheckLowRates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckLowRates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double error = 5.65;
+            String input;
+            int payrate;
+            Console.Write("Please enter hourly pay rate: ");
+            input = Console.ReadLine();
+            payrate = Convert.ToInt32(input);
+
+            if (payrate < error)
+                Console.WriteLine("error, payrate cannot be less that $5.65.");
+        }
+    }
+}
