ff1fa5b56d028cf9e4dd5d9f5fc7e8a6da8b6c7c
Werkheisera2 <werkheisera2@gmail.com>
2014-09-15 17:19:42 -0400

    Almost done with CheckLowandHighRates,cs

---
+++ CheckLowandHighRates.cs/CheckLowandHighRates.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckLowandHighRates.cs
+{
+    class LowHigh
+
+    {
+        static void Main(string[] args)
+        {
+            string rate;
+
+            Console.Write("Please enter your hourly pay rate. ");
+            rate=
+
+            if(rate < 5.65)
+             Console.WriteLine("Error. Please enter valid payrate.");
+            
+            else if(rate >  5.64)
+             Console.WriteLine("Thank you. :P");
+        }
+    }
+}
